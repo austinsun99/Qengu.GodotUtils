6f67f9e9f8f652b8e967242bfea0aa3779049c0f
austinsun99 <austinsun102@gmail.com>
2026-06-30 11:45:40 -0700

    feat: General node extensions for dealing with children

---
+++ src/Qengu.GodotUtils.Extensions/Nodes/NodeExtensions.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+namespace Qengu.GodotUtils.Extensions;
+
+public static class NodeExtensions
+{
+
+    /// <summary>
+    /// Returns the first child of the type T, or null if there aren't any
+    /// </summary>
+    /// <typeparam name="T">The type to find</typeparam>
+    /// <param name="includeInternal"> Whether or not to include internal nodes </param>
+    /// <returns>The child, or null if there are none</returns>
+    public static T? GetChildOrNull<T>(this Node node, bool includeInternal = false) where T : Node
+    {
+        foreach (var child in node.GetChildren(includeInternal))
+            if (child is T childAsT) return childAsT;
+        return null;
+    }
+
+    /// <summary>
+    /// Queue frees all children of the node. Does not free the node
+    /// </summary>
+    /// <param name="includeInternal"> Whether or not to include internal nodes </param>
+    public static void QueueFreeChildren(this Node node, bool includeInternal = false)
+    {
+        foreach (var child in node.GetChildren(includeInternal)) child.QueueFree();
+    }
+
+}
