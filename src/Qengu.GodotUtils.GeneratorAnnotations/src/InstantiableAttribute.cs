492298060a673d9f5af82abcfd28fa7adaa964e0
austinsun99 <austinsun102@gmail.com>
2026-06-30 16:10:31 -0700

    doc: update documentation to mention script must be in the same folder for InstantiableAttribute

--- src/Qengu.GodotUtils.GeneratorAnnotations/src/InstantiableAttribute.cs
+++ src/Qengu.GodotUtils.GeneratorAnnotations/src/InstantiableAttribute.cs
@@ -10,6 +10,7 @@ namespace Qengu.GodotUtils.GeneratorAnnotations;
 /// The parameter <paramref name="init"/> is used to specify the name of the Init function <br/>
 /// The parameter <paramref name="sceneName"/> is used to specify the name of the scene. If left
 /// uninitialized, it will default to the snake case of the script name. <br/>
+/// Note that the scene this script is attached to must be in the same folder as the script.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 sealed public class InstantiableAttribute(string init = "Init", string? sceneName = null) : Attribute
