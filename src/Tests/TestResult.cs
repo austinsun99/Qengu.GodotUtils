ac563b283043e8e6bb1df63cec29f3da328dc5f4
austinsun99 <austinsun102@gmail.com>
2026-07-05 00:22:10 -0700

    refactor: move TestRunner class to separate instantiable class. returns async ienumerable after each test

---
+++ src/Tests/TestResult.cs
@@ -0,0 +1,3 @@
+using Qengu.GodotUtils.Tests;
+
+public sealed record TestResult(bool Success, string FileName, string MethodName, string ExceptionMessage);
--- tests/testing/TestRunner.NodeTest.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Godot;
-using Qengu.GodotUtils.Tests;
-
-using static Qengu.GodotUtils.Tests.Assertions;
-
-public partial class TestRunner : Node
-{
-}
