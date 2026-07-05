6afbc74734d62c245f56353ee2e56ebe31ed7267
austinsun99 <austinsun102@gmail.com>
2026-07-05 00:50:04 -0700

    refactor: remove all logic to one attribute, and specify injection with paramters

--- tests/testing/tests/BasicTest.cs
+++ tests/testing/tests/BasicTest.cs
@@ -12,7 +12,7 @@ public class BasicTest
         Assert(1 == 2);
     }
 
-    [NodeTest]
+    [Test(InjectionType.ParentNode)]
     public async Task WaitTest(Node parent)
     {
         await parent.ToSignal(parent.GetTree().CreateTimer(5f), SceneTreeTimer.SignalName.Timeout);
